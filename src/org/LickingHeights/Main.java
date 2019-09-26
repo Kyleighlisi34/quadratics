3b27e54ed52d6053f4c8d9b6d4e1cfcf7044749b
Kyleigh <kyleigh.lisi.2020@student.lhschools.org>
2019-09-26 09:51:18 -0400

    added todo steps

--- src/org/LickingHeights/Main.java
+++ src/org/LickingHeights/Main.java
@@ -3,6 +3,76 @@ package org.LickingHeights;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        //todo declare variables a,b,c,discriminant,axis of symmetry,parabolaFacing,x,y
+
+        //todo initialize manually
+
+        // todo calculations
+
+        // todo print results
+
+        // todo not in code check results
+
+        //todo initialize scanner and redo the initialize area
+
+        //todo double check results 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
