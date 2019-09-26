f90f72b6c46e308bf15a174128837272a113acee
Kyleigh <kyleigh.lisi.2020@student.lhschools.org>
2019-09-26 09:57:22 -0400

    declare variables

--- src/org/LickingHeights/Main.java
+++ src/org/LickingHeights/Main.java
@@ -5,7 +5,8 @@ public class Main {
     public static void main(String[] args) {
 
         //todo declare variables a,b,c,discriminant,axis of symmetry,parabolaFacing,x,y
-
+        double a,b,c,discriminant,axisOfSysmmetry,x,y;
+        String parabolaFacing;
         //todo initialize manually
 
         // todo calculations
@@ -16,7 +17,7 @@ public class Main {
 
         //todo initialize scanner and redo the initialize area
 
-        //todo double check results 
+        //todo double check results
 
 
 
